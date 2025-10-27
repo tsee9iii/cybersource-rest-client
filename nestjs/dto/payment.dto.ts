bc3d6aa0f41660aba4669d0bfe4d9c83f64cba20
Tseegii <tserendorj@infinite.mn>
2025-10-27 10:06:01 +0800

    fix: customer create dto

--- nestjs/dto/payment.dto.ts
+++ nestjs/dto/payment.dto.ts
@@ -63,12 +63,57 @@ export interface CreateTokenDto {
 }
 
 export interface CreateCustomerDto {
+  /**
+   * The Id of the Customer Token.
+   * If not provided, CyberSource will generate one.
+   * @minLength 1
+   * @maxLength 32
+   */
+  id?: string;
   clientReferenceInformation?: ClientReferenceInformationDto;
-  buyerInformation?: BillToDto;
+  buyerInformation?: {
+    /**
+     * Your identifier for the customer.
+     * @maxLength 100
+     */
+    merchantCustomerID?: string;
+    /**
+     * Customer's primary email address, including the full domain name.
+     * @maxLength 255
+     */
+    email?: string;
+  };
+  objectInformation?: {
+    /**
+     * Name or title of the customer.
+     * @maxLength 60
+     */
+    title?: string;
+    /**
+     * Comments that you can make about the customer.
+     * @maxLength 150
+     */
+    comment?: string;
+  };
   merchantDefinedInformation?: Array<{
-    key: string;
-    value: string;
+    /**
+     * Possible values: data1, data2, data3, data4, sensitive1, sensitive2, sensitive3, sensitive4
+     */
+    name?: string;
+    /**
+     * The value for your merchant-defined data field.
+     * @maxLength 100
+     */
+    value?: string;
   }>;
+  defaultPaymentInstrument?: {
+    /** The Id of the Customers default Payment Instrument */
+    id?: string;
+  };
+  defaultShippingAddress?: {
+    /** The Id of the Customers default Shipping Address */
+    id?: string;
+  };
 }
 
 // Verification DTOs
